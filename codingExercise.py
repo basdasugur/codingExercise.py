1ebb2948b116c38ba7fe08913d75abe7d303f17c
Uğur Başdaş <99135103+basdasugur@users.noreply.github.com>
2023-06-19 20:09:24 +0300

    Create codingExercise.py

---
+++ codingExercise.py
@@ -0,0 +1,19 @@
+#Input two digit number
+two_digit_number = input("Type a two digit number: ")
+
+#check type of input and split
+print(type(two_digit_number))
+print(two_digit_number[0] + two_digit_number[1])
+
+
+#conversion string to integer
+sum_numbers0 = int(two_digit_number[0])
+sum_numbers1 = int(two_digit_number[1])
+
+
+# sum two integer input
+print(sum_numbers0 + sum_numbers1)
+
+
+
+
